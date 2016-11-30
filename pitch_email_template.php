be07cdaca61850a6fa13837dc32d58e0b00c04fa
Will Hay <williamfmhay@gmail.com>
2016-11-30 14:12:37 +0000

    initial files

---
+++ pitch_email_template.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Against the Tide</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header-container">
+				<div id="header">
+					<img src="email header 3.jpg" alt="The remarkable true story of an English nun, trapped behind enemy lines during the First World War"/>
+				</div>
+			</div>
+			<div id="intro-text-container">
+				<div id="intro-text" class="text">
+					<p>Dear James Hay,</p>
+					<p>We open on the vast moors in Scotland during the hot, hot summer of 1889. A teenage girl is discovered  running through the heather, her dress torn and her face a mess. She is striking with black hair and  piercing blues eyes. This is YOUNG MAY, our hero. In a series of quickfire scenes we establish that this is a very head strong young woman struggling with the constraints of Victorian life; fighting with her  mother about her freedom; fending off a male suitor who belittles her. But the mood changes suddenly  as we are transported to a cold stark room. YOUNG MAY sat alone. She looks terrified and close to  tears. A nun appears and proceeds to cut YOUNG MAY’s hair off. She then places a brand new  ‘cornette’ on YOUNG MAY’s head and tells her, ‘You have nothing to fear.’</p>
+				</div>
+			</div>
+			<div id="video-container">
+				<div id="video">
+					<iframe src="https://player.vimeo.com/video/192983875" width="640" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+				</div>
+			</div>
+			<div id="closing-text-container">
+				<div id="closing-text" class="text">
+					<p>THE WRITER NICK SUTTON started writing in his teens, while at Ampleforth College, and had two  theatre plays produced before he was 18. After a BA in Theatre Studies and a short career as an actor  in the UK, he returned to writing and won commissions from The Gateway Theatre, Chester, The  London New Play Festival (Old Red Lion and The Finborough Theatre), Louder Than Words  Productions (Young Vic Studio), The Australian Writer’s Guild and The Arts Council of Great Britain (The  People Show).</p>
+					<p>yours sincerely</p>
+					<p>Nick Sutton</p>
+				</div>
+			</div>
+			<div id="footer-container">
+				<div id="footer">
+					<div id="footer-text">
+						<a href="mailto:info@nicksuttorfilms.co.uk?Subject=Against%the%Tide">email: info@nicksuttonfilms.co.uk</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
+	<script src="script.js"></script>
+</html>
